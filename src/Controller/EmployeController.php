38b2829f37d16df9ea698ff226e9b990608f4ff0
Mohamed <162590718+mohars25@users.noreply.github.com>
2024-07-18 21:23:40 +0200

    Update EmployeController.php

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+
 use App\Repository\EmployeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
